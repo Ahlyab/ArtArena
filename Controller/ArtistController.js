e727ccad86777ea553ad5f101f66e810596e4bc9
Ahlyab <ahlyabasad@gmail.com>
2024-11-01 00:53:47 +0500

    artist  get by id updated

--- Controller/ArtistController.js
+++ Controller/ArtistController.js
@@ -196,13 +196,11 @@ module.exports.get_artist = async (req, res) => {
   }
 
   try {
-    const artist = await Artist.findById(id);
+    const artist = await Artist.findById(id).populate("arts");
     if (!artist) {
       return res.status(404).json({ message: "Artist not found", status: 404 });
     }
-    return res
-      .status(200)
-      .json({ artist: artist.populate("art"), status: 200 });
+    return res.status(200).json({ artist: artist, status: 200 });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ message: error.message, status: 400 });
